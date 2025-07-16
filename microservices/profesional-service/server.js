26d6ce86e907cc0f359ed3ba33281abb79925c0f
tiano <lucciano.caceres@alumnos.uv.cl>
2025-07-16 18:12:10 -0400

    commit: profesional service funcional

--- microservices/profesional-service/server.js
+++ microservices/profesional-service/server.js
@@ -1,1 +1,18 @@
-// Punto de entrada para iniciar este microservicio
+// microservices/paciente-ms/server.js
+require("dotenv").config();
+const app = require("./src/app");
+const { connectToDatabase } = require("./src/database/database");
+
+const port = process.env.PORT || 3001;
+
+async function main() {
+  await connectToDatabase();
+
+  app.listen(port, () => {
+    console.log(
+      `[Profesionales-MS] Servidor Express escuchando en http://localhost:${port}`
+    );
+  });
+}
+
+main();
