2ffcb3723f60854358a2e90d899d5a206cfb4f61
Angel Salgado <angel.salgado@alumnos.uv.cl>
2025-07-16 19:42:27 -0400

    Prueba 2

--- microservices/auth-ms/src/routes/auth.routes.js
+++ microservices/auth-ms/src/routes/auth.routes.js
@@ -5,6 +5,8 @@ import { verifyAuth } from '../middleware/verifyAuth.js'
 const router = express.Router()
 
 router.post('/login', login)
-router.get('/verify', verifyAuth, (req, res) => { res.json({ valid: true, user: req.user }) })
+router.get('/verify', verifyAuth, (req, res) => {
+    res.json({ valid: true, user: req.user }) 
+})
 
 export default router
